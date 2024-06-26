4e01025dc72998c4197ad3affe8046daf0662ecb
longvibol <vibollong@gmail.com>
2024-06-26 16:37:30 +0700

    ssee

--- code/java_reactive/src/main/java/java_reactive_demo/exercise/FileService.java
+++ code/java_reactive/src/main/java/java_reactive_demo/exercise/FileService.java
@@ -5,12 +5,21 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import java_reactive_demo.util.Util;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class FileService {
 	
 	public static Mono<String> read(String fileName){
 		return Mono.fromSupplier(() -> readFile(fileName));
+	}
+	
+	public static Flux<String> readLine(String fileName){
+		return Flux.generate(sink ->{
+			sink.next(readFile(fileName));
+			Util.sleep(1);
+		});
 	}
 	
 	private static String readFile(String fileName) {
